1758c55266cf17f85c1ec651864be8970d00b881
satyaniti singh <satyaniti.singh@gmail.com>
2022-07-23 23:29:39 +0530

    end of section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,12 +16,14 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        public readonly ITokenService _tokenService ;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
         [HttpPost("Register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto ){
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto ){
             if(await UserExists(registerDto.Username)){
                 return BadRequest("User Name is Taken");
             }
@@ -33,10 +36,13 @@ namespace API.Controllers
             } ;
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
         var user = await _context.Users
         .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
         
@@ -46,7 +52,10 @@ namespace API.Controllers
         for(int i =0; i< computedHash.Length; i++){
             if(computedHash[i]!= user.PasswordHash[i]) return Unauthorized("Invalid Password");
         }
-        return user;
+        return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
         private async Task<bool> UserExists(string username)
         {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbAccordionModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+
+    BsDropdownModule.forRoot(),
+    CommonModule,
+    BrowserAnimationsModule,
+    NgbAccordionModule,
+    NgbModule,
+    
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class SharedModule {}
